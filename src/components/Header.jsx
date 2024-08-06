a9aec6d312ab0ea329f795e174fb517221e30375
Mikolaj Lora <loradev@proton.me>
2024-08-06 22:43:43 +0200

    finishing Header: socialIcons and SCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import '../style/Header.scss';
+import SocialButtons from './SocialButtons';
 
 export default function Header() {
   return (
     <div className="header">
       <menu>
         <button className="button-menu">Home</button>
-        <button className="button-menu">Portfolio</button>
-        <button className="button-menu">About</button>
+        <button className="button-menu">Projects</button>
         <button className="button-menu">Landings</button>
+        <button className="button-menu">About</button>
         <button className="button-menu">Contact</button>
       </menu>
+      <SocialButtons />
     </div>
   );
 }
---
+++ src/components/SocialButtons.jsx
@@ -0,0 +1,24 @@
+import facebook from '../assets/icons/Facebook.png';
+import github from '../assets/icons/Github.png';
+import instagram from '../assets/icons/Instagram.png';
+import linkedin from '../assets/icons/Linkedin.png';
+
+export default function SocialButtons() {
+  const fb ='https://www.facebook.com/treadstoneassasin';
+  const git ='https://github.com/Ramox7171';
+  const insta ='https://www.instagram.com/ra.mox/';
+  const linked ='https://www.linkedin.com/in/mikolaj-lora/';
+
+
+  return (
+    
+    <div className='socialbuttons'>
+      <a href={fb}><img src={facebook} alt="facebook" /></a>
+      <a href={insta}><img src={instagram} alt="instagram" /></a>
+      <a href={git}><img src={github} alt="github" /></a>
+      <a href={linked}><img src={linkedin} alt="linkedin" /></a>
+   
+      </div>
+    
+  );
+}
