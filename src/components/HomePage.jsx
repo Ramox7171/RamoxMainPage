0fbd90832e1609c40957ab6943cb8d4b5546deae
Mikolaj Lora <loradev@proton.me>
2024-08-24 21:17:50 +0200

    working on homepage component -spining react logo added.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,16 @@
 import { homepageText } from '../assets/store/DATA';
 import '../style/HomePage.scss';
 import { NavLink } from 'react-router-dom';
+import reactlogo from '../assets/img/react_logo.png';
 
 
 export default function HomePage()
 {return <>
 
 <div className='home'>
+    <div className='react-logo'>
+        <img src={reactlogo} alt='react logo' />
+    </div>
 <h3>{homepageText}</h3>
 </div>
 <section className='home__section--links'>
