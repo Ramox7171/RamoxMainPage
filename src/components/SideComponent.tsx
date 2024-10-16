74a18ecbbf3b3157c9a186cf9681f06eaf9d17a0
Mikolaj Lora <loradev@proton.me>
2024-10-17 00:14:19 +0200

    Create modal component for side animation on Photo Section

---
+++ src/components/SideComponent.tsx
@@ -0,0 +1,62 @@
+import { useState, ReactNode } from 'react';
+import './SideComponent.css';
+import React from 'react';
+
+
+interface SideComponentProps {
+  side?: 'left' | 'right';  
+  btnName: string;
+  children?: ReactNode;    
+
+}
+
+const SideComponent: React.FC<SideComponentProps> = ({ side = 'right',btnName, children }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+
+  
+  const toggleMenu = () => {
+    if (isOpen) {
+      
+      setIsClosing(true);
+     const timeout = setTimeout(() => {
+        setIsOpen(false);
+        setIsClosing(false);
+     return clearTimeout(timeout) }, 500); 
+    } else {
+      setIsOpen(true);
+    }
+  };
+
+  return (
+    <div>
+      {/*  Menu */}
+      <button 
+        className={`side-component__button side-component__button--${side}`} 
+        onClick={toggleMenu}
+      >
+        {isOpen ? 'Close' : `${btnName}`}
+      </button>
+
+      {/* Menu boczne */}
+      <div 
+        className={`side-component__menu ${side} ${isOpen ? 'open' : ''} ${isClosing ? 'closing' : ''}`} 
+      >
+        {isOpen && (
+          <div className="side-component__content">
+            {children ? children : ( 
+              <ul className="side-component__list">
+                <li><a href="#portraits">Portraits</a></li>
+                <li><a href="#lifestyle">Lifestyle</a></li>
+                <li><a href="#studio">Studio</a></li>
+                <li><a href="#fashion">Fashion</a></li>
+              </ul>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SideComponent;
